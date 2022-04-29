e19d410699e0b9efbdbb4cdf845a7a703a5175e4
User <ray920707@gmail.com>
2022-04-29 15:51:16 +0800

    order examing

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,17 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed=delivery.price;
+    }
+    public int total(){
+        if (delivery.name.equals("Shopee")&& amount >= 199){
+            return amount;
+        }
+        return  amount+shipFeed;
     }
 }
